4a2bdc58353c398fd408fc5737295ce38ed3d550
Kirill <misterkirill666@gmail.com>
2024-02-03 18:53:28 +0500

    Add demotivator generation

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import re
 import os
+import io
 import string
 import discord
 import asyncio
 import random
+from glob import glob
 from discord import app_commands
 from dotenv import load_dotenv
+from PIL import Image, ImageFont, ImageDraw
 
 
 intents = discord.Intents.default()
@@ -16,6 +19,14 @@ tree = app_commands.CommandTree(client)
 
 words = []
 
+async def status_task():
+    while True:
+        if len(words) == 1:
+            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{len(words)} word'))
+        else:
+            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{len(words)} words'))
+        await asyncio.sleep(10)
+
 def generate_message():
     message = ''
 
@@ -23,6 +34,29 @@ def generate_message():
         message += random.choice(words) + ' '
 
     return message.rstrip()
+
+@tree.command(name='generate', description='Generate a message')
+async def generate(interaction: discord.Interaction):
+    await interaction.response.send_message(generate_message())
+
+@tree.command(name='demotivator', description='Generate a demotivator')
+async def generate(interaction: discord.Interaction):
+    path = random.choice(glob('data/images/*.png'))
+    image = Image.open(path)
+    image = image.resize((430, 334))
+
+    template = Image.open('demotivator.png')
+    template.paste(image, (72, 42))
+
+    draw = ImageDraw.Draw(template)
+
+    font = ImageFont.truetype("Impact.ttf", 32)
+    draw.text((template.width // 2, 450), generate_message(), (255, 255, 255), font=font, anchor='mm')
+
+    with io.BytesIO() as image_binary:
+        template.save(image_binary, 'PNG')
+        image_binary.seek(0)
+        await interaction.response.send_message(file=discord.File(image_binary, 'demotivator.png'))
 
 @client.event
 async def on_message(ctx: discord.Message):
@@ -43,31 +77,25 @@ async def on_message(ctx: discord.Message):
         else:
             with open('data/words.txt', 'a', encoding='utf8', buffering=1) as file:
                 file.write(word + '\n')
-
             words.append(word)
-
-@tree.command(name='generate', description='Generate a message')
-async def generate(interaction: discord.Interaction):
-    await interaction.response.send_message(generate_message())
-
-async def status_task():
-    while True:
-        if len(words) == 1:
-            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{len(words)} word'))
-        else:
-            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{len(words)} words'))
-        await asyncio.sleep(10)
+            print(f'New word from {ctx.author}: {word}')
+    
+    for attachment in ctx.attachments:
+        # I guess it should work
+        if attachment.content_type.startswith('image/'):
+            await attachment.save(f'data/images/{ctx.id}.png')
+            print(f'New image from {ctx.author}')
 
 @client.event
 async def on_ready():
-    await tree.sync()
-    client.loop.create_task(status_task())
-
     with open('data/words.txt', 'r', encoding='utf8') as file:
         for line in file.readlines():
             words.append(line.rstrip())
 
     print(f'Loaded {len(words)} words!')
+
+    await tree.sync()
+    client.loop.create_task(status_task())
 
 if __name__ == '__main__':
     load_dotenv()
